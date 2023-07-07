7452bd99acb004fc84fea747d1333c31f2b99989
Akshay Ghodki <akshayghodki520@gmail.com>
2023-07-07 12:29:50 +0530

    my first commit reactwebsite

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/akshay-ghodki-04a903233/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BUTH9K%2F37QsuP19BD5WJxVg%3D%3D" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://www.instagram.com/its__ghodki__6__19/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/akshayghodki" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
